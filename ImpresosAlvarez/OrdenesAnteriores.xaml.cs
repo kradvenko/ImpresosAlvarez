7fccc53aa13fbc6abf93979831e85ac1a6ea9b5d
kradvenko <38200946+kradvenko@users.noreply.github.com>
2023-04-11 15:16:38 -0700

    asdasd

--- ImpresosAlvarez/OrdenesAnteriores.xaml.cs
+++ ImpresosAlvarez/OrdenesAnteriores.xaml.cs
@@ -43,67 +43,75 @@ namespace ImpresosAlvarez
 
         private void dgOrdenesAnteriores_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
-            if (dgOrdenesAnteriores.SelectedItem != null)
+            try
             {
-                Ordenes Anterior = (Ordenes)dgOrdenesAnteriores.SelectedItem;
+                if (dgOrdenesAnteriores.SelectedItem != null)
+                {
+                    Ordenes Anterior = (Ordenes)dgOrdenesAnteriores.SelectedItem;
 
-                _Parent.tbTelefono.Text = Anterior.telefono;
-                _Parent.tbSolicita.Text = Anterior.solicitante;
-                _Parent.tbNombreTrabajo.Text = Anterior.nombre_trabajo;
-                _Parent.tbCantidad.Text = Anterior.cantidad.ToString();
-                _Parent.tbColorTintas.Text = Anterior.color_tintas;
-                _Parent.tbTipoPapel.Text = Anterior.tipo_papel;
-                _Parent.cbTamaño.Text = Anterior.tamano;
-                _Parent.cbPrimeraCopia.Text = Anterior.copia_1;
-                _Parent.cbSegundaCopia.Text = Anterior.copia_2;
-                _Parent.cbTerceraCopia.Text = Anterior.copia_3;
-                _Parent.cbCuartaCopia.Text = Anterior.copia_4;
-                _Parent.tbCostoAnterior.Text = Anterior.costo_anterior.ToString();
-                _Parent.tbNotas.Text = Anterior.otros_4;
-                _Parent.tbDescripcion.Text = Anterior.especificaciones;
-                if (Anterior.pegado == "SI")
-                {
-                    _Parent.chbPegado.IsChecked = true;
-                }
-                else
-                {
-                    _Parent.chbPegado.IsChecked = false;
-                }
-                if (Anterior.engrapado == "SI")
-                {
-                    _Parent.chbEngrapado.IsChecked = true;
-                }
-                else
-                {
-                    _Parent.chbEngrapado.IsChecked = false;
-                }
-                if (Anterior.perforacion == "SI")
-                {
-                    _Parent.chbPerforacion.IsChecked = true;
-                }
-                else
-                {
-                    _Parent.chbPerforacion.IsChecked = false;
-                }
-                if (Anterior.rojo == "SI")
-                {
-                    _Parent.chbRojo.IsChecked = true;
-                }
-                else
-                {
-                    _Parent.chbRojo.IsChecked = false;
-                }
-                if (Anterior.blanco == "SI")
-                {
-                    _Parent.chbBlanco.IsChecked = true;
-                }
-                else
-                {
-                    _Parent.chbBlanco.IsChecked = false;
-                }
-                _Parent.RutaOrden = Anterior.ruta;
+                    _Parent.tbTelefono.Text = Anterior.telefono;
+                    _Parent.tbSolicita.Text = Anterior.solicitante;
+                    _Parent.tbNombreTrabajo.Text = Anterior.nombre_trabajo;
+                    _Parent.tbCantidad.Text = Anterior.cantidad.ToString();
+                    _Parent.tbColorTintas.Text = Anterior.color_tintas;
+                    _Parent.tbTipoPapel.Text = Anterior.tipo_papel;
+                    _Parent.cbTamaño.Text = Anterior.tamano;
+                    _Parent.cbPrimeraCopia.Text = Anterior.copia_1;
+                    _Parent.cbSegundaCopia.Text = Anterior.copia_2;
+                    _Parent.cbTerceraCopia.Text = Anterior.copia_3;
+                    _Parent.cbCuartaCopia.Text = Anterior.copia_4;
+                    _Parent.tbCostoAnterior.Text = Anterior.costo_anterior.ToString();
+                    _Parent.tbNotas.Text = Anterior.otros_4;
+                    _Parent.tbDescripcion.Text = Anterior.especificaciones;
 
-                this.Close();
+                    if (Anterior.pegado == "SI")
+                    {
+                        _Parent.chbPegado.IsChecked = true;
+                    }
+                    else
+                    {
+                        _Parent.chbPegado.IsChecked = false;
+                    }
+                    if (Anterior.engrapado == "SI")
+                    {
+                        _Parent.chbEngrapado.IsChecked = true;
+                    }
+                    else
+                    {
+                        _Parent.chbEngrapado.IsChecked = false;
+                    }
+                    if (Anterior.perforacion == "SI")
+                    {
+                        _Parent.chbPerforacion.IsChecked = true;
+                    }
+                    else
+                    {
+                        _Parent.chbPerforacion.IsChecked = false;
+                    }
+                    if (Anterior.rojo == "SI")
+                    {
+                        _Parent.chbRojo.IsChecked = true;
+                    }
+                    else
+                    {
+                        _Parent.chbRojo.IsChecked = false;
+                    }
+                    if (Anterior.blanco == "SI")
+                    {
+                        _Parent.chbBlanco.IsChecked = true;
+                    }
+                    else
+                    {
+                        _Parent.chbBlanco.IsChecked = false;
+                    }
+                    _Parent.RutaOrden = Anterior.ruta;
+
+                    this.Close();
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("ERROR ORDEN ANTERIOR: " + exc.Message);
             }
         }
     }
